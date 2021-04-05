fba19d65857a014df62efd085f54784d3c4596d2
agwanyaeen <yaseen04071998@gmail.com>
2021-04-05 21:41:22 +0530

    remove dead code

--- lib/main.dart
+++ lib/main.dart
@@ -39,48 +39,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // final height = MediaQuery.of(context).size.height;
-    // final width = MediaQuery.of(context).size.width;
-    return build1(context);
-    final paddingSize = getSize(context);
-
-    final app1 = Scaffold(
-      body: Container(
-        // height: height,
-        // width: width,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('/images/background.jpg'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: SingleChildScrollView(
-          child: Container(
-            padding: EdgeInsets.fromLTRB(paddingSize, 50, paddingSize, 0),
-            child: Wrap(
-              children: [
-                Intro(),
-                SizedBox(
-                  height: 15,
-                ),
-                AboutMe(),
-                SizedBox(
-                  height: 15,
-                ),
-                // Experience(),
-                SizedBox(
-                  height: 15,
-                ),
-                Qualification(),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget build1(BuildContext context) {
     final paddingSize = getSize(context);
     return Scaffold(
       body: SmoothScrollWeb(
--- lib/widgets/presentation/contact_me.dart
+++ lib/widgets/presentation/contact_me.dart
@@ -1,19 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio/widgets/bootstrap_card.dart';
 
-import '../contact.dart';
-
 class ContactMe extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BootstrapCard(
       title: 'Contact Me',
-      child: Column(
-          // ContactCard(
-          //   title: 'Email',
-          //   content: 'yaseen04071998@gmail.com',
-          // ),
-          ),
+      child: Column(),
     );
   }
 }
